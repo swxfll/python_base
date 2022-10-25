37ee4977dd9dbeaa5a5e7f6bdd9a6307bd428511
swxfl <swxfl@163.com>
2022-10-25 22:27:51 +0800

    init

---
+++ type.py
@@ -0,0 +1,20 @@
+# 整数转字符串
+num_str = str(11)
+print(type(num_str), num_str)
+
+# 浮点数转字符串
+float_str = str(3.14)
+print(type(float_str), float_str)
+
+# 字符串转整数
+num = int("11")
+print(type(num), num)
+
+# 字符串转浮点数
+num2 = float("3.1415")
+print(type(num2), num2)
+
+# 浮点数转整数
+int_num = int(9.9999)
+print(type(int_num), int_num)
+
