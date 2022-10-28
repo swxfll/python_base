7887bff861165ff45de9e45bf37e4a85ef7fce28
swxfl <16606652035@163.com>
2022-10-28 18:54:57 +0800

    完善for循环和新增函数

--- 第五章/函数之西游ATM.py
+++ 第五章/函数之西游ATM.py
@@ -2,6 +2,9 @@
 account_money = 10000
 user_name = None
 
+# 要求输入姓名
+user_name = input("请输入你的姓名: ")
+
 
 # 定义主菜单
 def menu():
@@ -11,7 +14,6 @@ def menu():
     print("存款    [输入2]")
     print("取款    [输入3]")
     print("退出    [输入4]")
-
     return int(input("请输入您的选择"))
 
 
@@ -55,25 +57,9 @@ while True:
         saving(num)
         continue
     elif user_input == 3:
-
-
-user_name = input("请输入你的姓名: ")
-
-# while True:
-#     select = menu(user_name)
-#     if select == 1:
-#         print("---------------查询余额-----------")
-#         print("%s, 你好, 你的账户余额是: %d" % (user_name, account_money))
-#     elif select == 2:
-#         num = int(input("请输入你要存钱的数量: "))
-#         account_money += num
-#         print(f"成功存入{num}元, 账户余额{account_money}")
-#     elif select == 3:
-#         num = int(input("请输入取款金额: "))
-#         account_money -= num
-#         print(f"成功取出{num}, 账户余额{account_money}")
-#     elif select == 4:
-#         print("Bye")
-#         break;
-#     else:
-#         print("你的输入有误, 请重新输入")
+        num = int(input("请输入取款金额: "))
+        get_money(num)
+        continue
+    else:
+        print("程序退出")
+        break
